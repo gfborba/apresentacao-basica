edad5e8a139c728deafe8f10ee58748d13fcdcaf
Guilherme Borba <guifborba@hotmail.com>
2022-04-18 18:37:16 -0300

    onclick

--- site/sjs.js
+++ site/sjs.js
@@ -1,2 +1,11 @@
+let man = document.getElementById ('mani')
+
 function info() {
+    man.innerHTML = ` Meu nome é <strong>Guilherme Ferreira de Borba</strong>, atualmente sou estudante de Administração Pública na <br> UDESC (Universidade do Estado de Santa Catarina) em Florianópolis.
+    </p>
+    <p>
+Recentemente, por recomendações de amigos próximos, comecei a estudar do zero HTML5 e CSS3, para ser inserido no mundo dev. Hoje, tenho o básico de conhecimento em HTML, CSS, JavaScript, Git e Github.
+    </p>
+    <p>
+A minha intenção é um dia, ser um desenvolvedor Full-Stack e seguir carreira no ramo. Portanto, caso o sonho não dê certo, estou me divertindo no processo :)`
 }
